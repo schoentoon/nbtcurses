3b2986fe0be931775bb7ff5cfce1296b7a936785
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-07-10 18:13:17 +0200

    Made the help window slightly more flexible
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- src/help_window.c
+++ src/help_window.c
@@ -9,22 +9,60 @@
 
 #include "help_window.h"
 
+#include <string.h>
 #include <ncurses.h>
 
-#define HEIGHT (5/* amount of lines */ + 2)
-#define WIDTH (56/* longest line */ + 3)
+static const char* help[] =
+  { "Navigation:"
+  , " Up, down, page up and page down can be used to navigate."
+  , " You can also use the mouse scroll wheel."
+  , "Editing:"
+  , " To edit the current selected item simply press 'e'"
+  , "Searching:"
+  , " To search press 'f' and just start typing, it'll search on keys only at this moment."
+  , NULL };
+
+static const attr_t line_attrs[] =
+  { A_BOLD
+  , 0
+  , 0
+  , A_BOLD
+  , 0
+  , A_BOLD
+  , 0 };
+
+inline size_t amount_of_lines() {
+  size_t i = 0;
+  while (help[i])
+    i++;
+  return i;
+};
+
+inline size_t longest_line() {
+  size_t i = 0;
+  const char* line = help[i];
+  size_t longest = 0;
+  while (line) {
+    size_t len = strlen(line);
+    if (len > longest)
+      longest = len;
+    line = help[++i];
+  }
+  return longest;
+};
+
+#define HEIGHT (amount_of_lines() + 2)
+#define WIDTH (longest_line() + 3)
 
 void show_help_window() {
   WINDOW* w = newwin(HEIGHT, WIDTH, (LINES - HEIGHT) / 2, (COLS - WIDTH) /2);
-  wattron(w, A_BOLD);
-  mvwprintw(w, 1, 1, "Navigation:");
-  wattroff(w, A_BOLD);
-  mvwprintw(w, 2, 2, "Up, down, page up and page down can be used to navigate.");
-  mvwprintw(w, 3, 2, "You can also use the mouse scroll wheel.");
-  wattron(w, A_BOLD);
-  mvwprintw(w, 4, 1, "Editing:");
-  wattroff(w, A_BOLD);
-  mvwprintw(w, 5, 2, "To edit the current selected item simply press 'e'");
+  size_t i = 0;
+  while (help[i]) {
+    wattron(w, line_attrs[i]);
+    mvwprintw(w, i + 1, 1, help[i]);
+    wattroff(w, line_attrs[i]);
+    i++;
+  };
   box(w, 0, 0);
   mvwprintw(w, HEIGHT-1, 1, "Press any key to get rid of this window.");
   wrefresh(w);
